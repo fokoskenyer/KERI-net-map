6a7f2b12d6c7d42c6733c962c30cb92ad2921344
fokoskenyer <86595462+fokoskenyer@users.noreply.github.com>
2023-10-09 21:29:41 +0200

    finish

--- src/data/js/map.js
+++ src/data/js/map.js
@@ -1,32 +1,36 @@
-const svg = document.getElementById("Layer_1");
-const centerX = svg.clientWidth / 2;
-const centerY = svg.clientHeight / 2;
-
-let isDragging = false;
-let previousX, previousY;
-let currentScale = 1;
+const fosz = document.getElementById("fsz");
 
 
 
+function changeStory(story) {
+  if (story === "fsz") {
+    fosz.style.width = "1490px";
+    fosz.style.position = "fixed";
+    fosz.style.marginLeft = "10%"
+    fosz.style.marginTop = "0px";
+    fosz.innerHTML = `<img src="data/img/fsz.svg" alt="">`;
+} else if (story === "it") {
+    fosz.style.width = "1100px";
+    fosz.style.position = "fixed";
+    fosz.style.marginLeft = "21%";
+    fosz.style.marginTop = "150px";
+    fosz.innerHTML = `<img src="data/img/it.svg" alt="">`;
+} else if (story === "1m") {
+    fosz.style.width = "2000px";
+    fosz.style.marginLeft = "7%";
+    fosz.style.paddingRight = "4%";
+    fosz.innerHTML = `<img src="data/img/1m.svg" alt="">`;
 
-function test(story) {
-  if (story == "fsz") {
-    document.getElementById("fsz").style.display = "block";
-    document.getElementById("it").style.display = "none";
-  } 
-  if (story == "it") {
-    document.getElementById("fsz").style.display = "none";
-    document.getElementById("it").style.display = "block";
-  } 
-  if (story == "1m") {
-
-  } else {
-    
-  }
-
+} else if (story === "2m") {
+    fosz.style.width = "2000px";
+    fosz.style.marginLeft = "7%";
+    fosz.style.paddingRight = "4%";
+    fosz.innerHTML = `<img src="data/img/2m.svg" alt="">`;
+}
 }
 
 //blue button
+//DONT WORK NEEDS FIXING
 //DONT WORK NEEDS FIXING
 document.addEventListener("DOMContentLoaded", function () {
     const buttons = document.querySelectorAll(".button");
@@ -41,37 +45,4 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 //DONT WORK NEEDS FIXING
-
-
-svg.addEventListener("wheel", (event) => {
-    event.preventDefault();
-    const zoomAmount = event.deltaY > 0 ? 0.9 : 1.1;
-    currentScale *= zoomAmount;
-    
-    svg.style.transformOrigin = `${centerX}px ${centerY}px`;
-    svg.style.transform = `scale(${currentScale})`;
-});
-
-
-
-
-svg.addEventListener("mousedown", (event) => {
-    isDragging = true;
-    previousX = event.clientX;
-    previousY = event.clientY;
-});
-document.addEventListener("mouseup", () => {
-    isDragging = false;
-});
-document.addEventListener("mousemove", (event) => {
-    if (isDragging) {
-        const deltaX = event.clientX - previousX;
-        const deltaY = event.clientY - previousY;
-        
-        svg.viewBox.baseVal.x -= deltaX / currentScale;
-        svg.viewBox.baseVal.y -= deltaY / currentScale;
-        
-        previousX = event.clientX;
-        previousY = event.clientY;
-    }
-});
+//DONT WORK NEEDS FIXING
